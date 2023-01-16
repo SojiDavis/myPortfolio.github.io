49614def9a3ebb54ca22b952cfb4c7c8f0ee5e55
SojiDavis <122748953+SojiDavis@users.noreply.github.com>
2023-01-16 19:17:39 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+//install service worker
+self.addEventListener('install', e =>{
+       e.waitUntil(
+            cashes.open("static").then (cache=>{
+                return cache.addAll(["./",
+                "./css/style.css",
+                "./css/academic.css",
+                "./css/contact.css",
+                "./css/curncy_style.css",
+                "./css/forcast_style.css",
+                "./css/testimonial.css",
+                "./files/forcasting_value_table.csv",
+                "./files/Resume_Soji Davis.pdf",
+                "./images/academic.png",
+                "./images/Currency_converter.png",
+                "./images/Fuel_forecaster.png",
+                "./images/Resume_image.png",
+                "./images/Currency_converter1.jpg",
+                "./images/Fuel_forcaster1.jpg",
+                "./images/soji.jpg",
+                "./images/logo.svg",
+                "./js/converter.js",
+                "./js/forcast.js",
+                "./js/pw.js",
+                "./js/script.js",
+                "./academic.html",
+                "./contact.html",
+                "./Curncy_cnvrter.html",
+                "./forcasting.html",
+                "./index.html",
+                "./professional.html",
+                "./Skill_pages.html",
+                "./testimonial.html",
+                "./manifest.json",
+                "./sw.js"                     
+                ]);
+            })
+       );
+
+});
+
+//activate Service Worker
+self.addEventListener('activate', e =>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request)
+        })
+    );
+});
+
+this.addEventListener('fetch',  {
+    // it can be empty if you just want to get rid of that error
+});
